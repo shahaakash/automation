4e11c83be22afc44606db04099337bcb81ee7496
shahaakash <meaashah@gmail.com>
2016-01-28 15:25:58 +0530

    Create BasicSelenium.java

---
+++ BasicSelenium.java
@@ -0,0 +1,18 @@
+// Define Package Name
+package wpAdmin;
+
+// Import Selenium Libraries
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+// Define Class Name
+public class BasicStructure {
+
+	public static void main(String[] args) {
+		
+    //  Selenium WebDriver Commands
+
+	}
+}
